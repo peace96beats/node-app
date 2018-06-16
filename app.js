eaf105bdbabff1240ebf387693de6efa70ecd687
atsushi <peace96beats@gmail.com>
2018-06-16 19:13:41 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+var server = http.createServer(
+    (request,response)=>{
+        response.setHeader('Content-Type', 'text/html');
+        response.write('<!DOCTYPE html><html lang="ja">');
+        response.write('<head><meta charset="UTF-8">');
+        response.write('<title>Hello</title></head>');
+        response.write('<body><h1>Hello Node.js!</h1>');
+        response.write('<p>これは、Node.jsのサンプルページです</p>', 'utf8');
+        response.write('</body></html>'); 
+        response.end();
+    }
+);
+
+server.listen(3000);
+console.log('Server start!');
