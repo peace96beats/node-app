a503f9434c329298338b75270508b8a08b59fadb
atsushi <peace96beats@gmail.com>
2018-06-22 01:26:11 +0900

    html テキストの操作

--- app.js
+++ app.js
@@ -9,15 +9,16 @@ console.log('Server start!');
 // こっこまでメインプログラム
 
 // createServer の処理
-function getFromClient(req,res){
-    request = req;
-    response = res;
-    fs.readFile('./index.html', 'UTF-8', writeToResponse);
-}
+function getFromClient(request,response){
+    fs.readFile('./index.html', 'UTF-8', 
+    (error,data)=>{
+        var content = data.
+            replace(/dummy_title/g, 'たいとるです').
+            replace(/dummy_content/g, 'これがコンテンツです');
 
-//readRile完了後の処理
-function writeToResponse(error,data){
-    response.writeHead(200,{'Content-Type': 'text/html'});
-    response.write(data);
-    response.end();
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(content);
+        response.end();
+    }
+    );
 }
